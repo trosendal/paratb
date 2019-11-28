6cfe2b2bca53b777e48a01ca1f934594c0544af0
Thomas Rosendal <thomas.rosendal@sva.se>
2019-11-28 23:56:44 +0100

    Added seeding of herds and a plot of the herd level infection

--- paratb/vignette/sample.R
+++ paratb/vignette/sample.R
@@ -7,15 +7,38 @@ library(paratb)
 ## to change the starting state replace u0. In the real model we would
 ## use a list of herds in the population and the number of animals in
 ## each at the beginning of the study period:
-model <- paratb(tspan = seq(1, 10*365))
+model <- paratb(tspan = seq(1, 10*365, 90))
 
-## Add 10 infected cows in a herd to start the outbreak
-model <- seed_herd(model, 1, "cow", 10)
+## Seed infection in 0.2% of the herds
+for(i in seq_len(ncol(model@u0)*0.002)) {
+model <- seed_herd(model,
+                   i,
+                   "cow",
+                   ceiling(model@u0["S3",i] * 0.02))
+model <- seed_herd(model,
+                   i,
+                   "heifer",
+                   ceiling(model@u0["S2",i] * 0.02))
+model <- seed_herd(model,
+                   i,
+                   "calf",
+                   ceiling(model@u0["S1",i] * 0.02))
+}
 
-## Add 10 million random events to the model. This will just inject random
+## Add 5 million random events to the model. This will just inject random
 ## trade events between herds; in the real model we would use recorded
 ## animal movements here to reflect the real population.
 model <- random_events(model, 1000000)
 
-## Run the model to see the spread
-df <- trajectory(run(model))
+## Run the model
+result <- run(model)
+
+## Just extract the data we need about the herd status at each time point
+df <- counti(result, "true")
+
+plot(df$time,
+     df$count,
+     type = "s",
+     ylim = c(0, max(df$count)),
+     ylab = "Count of positive herds",
+     xlab = "Time in days")
